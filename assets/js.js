5b773e3932044ae26956762fd413f35b9193a9e1
dungyourluv <corepper03@gmail.com>
2021-08-30 20:39:49 +0700

    up

--- assets/js.js
+++
@@ -1,6 +0,0 @@
-/* nav */
-var btnElementa = document.querySelector('.header__nav-menu-button');
-var showNav = document.querySelector('.nav-second__main');
-    btnElementa.onclick = function () {
-        showNav.classList.toggle('open__nav');
-    }
