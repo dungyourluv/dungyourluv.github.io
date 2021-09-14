528f95124fe9785f35aa508a325c5a3fb2ef0080
lethdung <84926848+dungyourluv@users.noreply.github.com>
2021-09-14 11:31:15 +0700

    upg

--- assets/html/assets/app.js
+++ assets/html/assets/app.js
@@ -8,9 +8,15 @@ var skillContent = document.querySelector(".skills-content");
 var textHomeHeading = document.querySelector(".home-heading");
 var homeContent = document.querySelector(".home-content");
 var homeImg = document.querySelector(".home-img");
-var moreContent = document.querySelectorAll(".more-content");
+var moreContent = document.querySelector(".more-content");
 var resume = document.querySelector(".resume");
 var homeMail = document.querySelector(".home-mail");
+var playlists = document.querySelector(".playlists");
+var moreChoose = document.querySelectorAll(".add--more");
+var playIcon = document.querySelector(".start");
+var pauseIcon = document.querySelector(".pause--icon");
+var audioContainer = document.querySelector(".audio-container");
+var listMusics = document.querySelector(".play-list");
 console.log("hello");
 /* main */
 function start() {
@@ -47,6 +53,7 @@ function removeEfHeader() {
 function skills() {
   removeHome();
   removeResume();
+  removePlayList();
   skill.classList.remove("opc-hidden");
   setTimeout(() => {
     skill.style.display = "block";
@@ -62,11 +69,11 @@ function Home() {
   setTimeout(() => {
     home.style.display = "block";
   }, 200);
+  removePlayList();
 }
 
 function resumeOn() {
   resume.classList.remove("opc-hidden");
-  console.log("resume");
 }
 
 function removeResume() {
@@ -115,15 +122,127 @@ window.onload = () => {
 };
 
 function moreBtn() {
-  moreContent.forEach((btn, index) => {
+  moreChoose.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      console.log(btn);
       removeHome();
       removeEfHeader();
       removeSkill();
       if (index == 0) {
+        removePlayList();
         resumeOn();
+      }
+      if (index == 1) {
+        playlistOn();
       }
     });
   });
+}
+
+function playlistOn() {
+  playlists.classList.remove("opc-hidden");
+  resume.classList.add("opc-hidden");
+  addListMusic();
+}
+
+function removePlayList() {
+  playlists.classList.add("opc-hidden");
+}
+
+function controlPlays(song) {
+  playIcon.addEventListener("click", () => {
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "initial";
+    song.play();
+  });
+  pauseIcon.addEventListener("click", () => {
+    pauseIcon.style.display = "none";
+    playIcon.style.display = "initial";
+    song.pause();
+  });
+}
+
+/* list music */
+class Music {
+  constructor(name, img, url, author) {
+    (this.name = name),
+      (this.img = img),
+      (this.url = url),
+      (this.author = author);
+  }
+}
+var MusicList = [
+  new Music(
+    "Yêu xa",
+    "./assets/imgs/image.jpg",
+    "./assets/audio/yeuxa.mp3",
+    "Vũ Cát Tường"
+  ),
+  new Music(
+    "Hẹn Một Mai",
+    "./assets/imgs/image1.jpg",
+    "./assets/audio/henmotmai.mp3",
+    "Vũ Cát Tường"
+  ),
+  new Music(
+    "Lemon tree",
+    "./assets/imgs/image.jpg",
+    "./assets/audio/lemontree.mp3",
+    "Vũ Cát Tường"
+  ),
+];
+function addListMusic() {
+  MusicList.forEach((music, index) => {
+    audioContainer.innerHTML += `<audio src=" ${music.url} " data-number="${index}"></audio>`;
+    listMusics.innerHTML += ` <div class="play-list-container">
+     <div class="list-img">
+       <img src="${music.img}" alt="">
+     </div>
+     <div class="list-info">
+       <h1 class="song-name">${music.name}</h1>
+       <span>${music.author}</span>
+     </div>`;
+  });
+  var songItem = document.querySelectorAll(".play-list-container");
+  var song = document.querySelectorAll(".audio-container > audio");
+  var header = document.querySelector(".play-header > p");
+  var nextSong = document.querySelector(".next--song");
+  var backSong = document.querySelector(".back--song");
+
+  function NextBackSong(next, back) {
+    var indexNext = next;
+    var indexBack = back;
+    nextSong.addEventListener("click", () => {
+      if (next < songItem.length) {
+        musicInListPlay();
+      }
+    });
+
+    backSong.addEventListener("click", () => {});
+  }
+
+  function musicInListPlay() {
+    songItem.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        NextBackSong(index + 1, index - 1);
+        let nameSong = btn.querySelector(".song-name").innerText;
+        header.innerText = nameSong;
+        playMusic(index);
+      });
+    });
+  }
+  musicInListPlay();
+  function playMusic(i) {
+    songItem.forEach((a) => {
+      a.classList.remove("play--hover");
+    });
+    song.forEach((s) => {
+      s.pause();
+    });
+    song[i].play();
+    controlPlays(song[i]);
+
+    songItem[i].classList.add("play--hover");
+    playIcon.style.display = "none";
+    pauseIcon.style.display = "initial";
+  }
 }
